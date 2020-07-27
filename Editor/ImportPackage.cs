8050e35cad58a253fb616680a17dc8ab9a34586e
Sofia Brown <sofia.brown@unity3d.com>
2020-07-27 15:51:51 -0500

    sample test

---
+++ Editor/ImportPackage.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using UnityEditor;
+
+public class ImportPackage : Editor
+{
+    [MenuItem("Tools/Import .unitypackage directly from Package")]
+    private static void ImportPackageDirectly()
+    {
+        string packageFullPath = Path.GetFullPath("Packages/com.unity.enginesupport.sampletest/resource/SomePackage.unitypackage");
+        AssetDatabase.ImportPackage(packageFullPath, true);
+    }
+}
---
+++ Runtime/TestRunner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TestRunner : NUnit.Framework.Interfaces.ITestListener
+{
+    public void TestStarted(NUnit.Framework.Interfaces.ITest test)
+    {
+        Debug.Log("Test started");
+    }
+
+    public void TestFinished(NUnit.Framework.Interfaces.ITestResult result)
+    {
+        Debug.Log("Test finished");
+    }
+
+    public void TestOutput(NUnit.Framework.Interfaces.TestOutput output)
+    {
+        Debug.Log("Test output");
+    }
+}
